c78bb20d00a87b9b8179e2cd3c4e2eb875e50c51
kim, jiwon <wo41896804@gmail.com>
2022-02-05 13:30:27 +0900

    0204_모의고사(2)
    정답 추가

--- exam.js
+++ exam.js
@@ -25,6 +25,27 @@ function solution(answers) {
     })
 }
 
+// 정답
+function solution(answers) {
+    let maxAnswers = []; // 최대 정답자
+    let scores = [0, 0, 0]; // 맞은 개수
+    let a = [ // 수포자의 찍는 방법 패턴
+        [1, 2, 3, 4, 5], // 5
+        [2, 1, 2, 3, 2, 4, 2, 5], // 8 
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // 10
+    ];
+    
+    for (let i=0; i<answers.length; i++) {
+      a[0][i % 5] === answers[i] ? scores[0]++ : false;
+      a[1][i % 8] === answers[i] ? scores[1]++ : false;
+      a[2][i % 10] === answers[i] ? scores[2]++ : false;
+    }
+
+    let max = Math.max(...scores); // 최고 점수
+    scores.map((v, i) => max === v ? maxAnswers.push(i+1) : false);
+    return maxAnswers;
+}
+
 let test1 = [1, 2, 3, 4, 5];
 // return [1]
 
