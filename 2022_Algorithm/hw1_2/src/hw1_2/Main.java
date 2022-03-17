f622b98a7c8193cb738f54c06e82f68013bbbcd3
kim, jiwon <wo41896804@gmail.com>
2022-03-17 16:11:04 +0900

    [hw1_2] reservation(2)
    종료시간
    -> 시작 시간에서 1~6시간 X
    -> 시작 시간에서 최소 2시 ~ 6시까지 O
    ex) 시작 시간 4시간 -> 종료시간 최소 5시(1시간) or 최대 6시(2시간)

--- 2022_Algorithm/hw1_2/src/hw1_2/Main.java
+++ 2022_Algorithm/hw1_2/src/hw1_2/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		// (2) 예약 번호는 1, 2, 3..., 시작시간, 종료시간은 랜덤 값인 예약 객체를 n개 생성하여 배열에 저장
 		for(int i=0; i<reservation.length; i++) {
 			int startTime = (int) (Math.random()*5+1); // 시작 시간: 1~5
-			int endTime = (int) (startTime + (Math.random()*5+1)); // 종료 시간: 시작 시간+1~6
+			int endTime = (int) (Math.random()*(6-(startTime+1)+1) + startTime+1); // 종료 시간: 시작 시간+1~6
 			reservation[i] = new Reservation(i+1, startTime, endTime);
 		}
 
@@ -21,12 +21,11 @@ public class Main {
 		}
 
 		// (4) 각 길이의 예약이 몇개씩인지 구함 (중첩 반복문을 사용하지 말고 수행시간 복잡도가 O(n)이 되도록 작성해 볼 것)
-
 		int[] count = new int[5]; // 예약 갯수 카운트 변수
 		for(int i=0; i < reservation.length; i++) {
 			count[reservation[i].getLength() - 1]++;
 		}
-
+		
 		// (5) 각 길이의 예약 수를 출력
 		for(int i=0; i<count.length; i++) {
 			System.out.println(i+1 + "시간 = "+ count[i]);
