49489a3f8030f1df13764c513255dc42186bb9f0
kim, jiwon <wo41896804@gmail.com>
2022-05-12 16:18:22 +0900

    [lab9_1] 백준 테스트

---
+++ 2022_Algorithm/lab9_1/src/lab9_1/Main.java
@@ -0,0 +1,14 @@
+package lab9_1;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a+b);
+		scanner.close();
+	}
+}
